1ffe66598a7e618b58e5a51d065cf53c283c9244
Victor Albarado Braga <99058838+VictorAlbaradoBraga@users.noreply.github.com>
2022-02-23 22:58:13 -0400

    Update main.py
    - Alteração nas classes
    - Adicionado posição dos players na tela

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import pygame
 import config
 
-
 pygame.init()
 
-
 # screen
-size = (1000, 600)
+size = (1000, 700)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Game")
 
@@ -15,14 +13,17 @@ game_loop = True
 game_clock = pygame.time.Clock()
 
 while game_loop:
-    screen.fill(config.Config.COLOR_BLACK)
+    screen.fill(config.Colors.COLOR_BLACK)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_loop = False
 
+    # drawing the objects
+    screen.blit(config.Players.player_1, config.Players.player_1_cord)
+    screen.blit(config.Players.player_2, config.Players.player_2_cord)
     # update screen
     pygame.display.flip()
-    game_clock.tick(config.Config.CLOCK_TICK)
+    game_clock.tick(config.Clock.CLOCK_TICK)
 
 pygame.quit()
