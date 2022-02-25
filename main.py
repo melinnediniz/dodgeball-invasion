6978df6e01cf1045cd82e7180a3ef064fbc3c89a
Johann Mayos(요한) <42842871+johannmay@users.noreply.github.com>
2022-02-24 20:42:58 -0400

    Controles basicos do player 1 adicionados

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import pygame
 import config
-from sys import exit
 from players import Players
-import paddle
+
 
 pygame.init()
 
@@ -14,21 +13,45 @@ pygame.display.set_icon(pygame.image.load('img/icon.png'))
 # game clock (FPS)
 game_clock = pygame.time.Clock()
 
+# control var of sprite coordinates
+sprite_x = Players.player_1_x
+sprite_y = Players.player_1_y
+
 while config.game_loop:
     screen.fill(config.Colors.COLOR_BLACK)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             config.game_loop = False
-            exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                sprite_x += 30
+            elif event.key == pygame.K_LEFT:
+                sprite_x -= 30
+            elif event.key == pygame.K_UP:
+                sprite_y -= 30
+            elif event.key == pygame.K_DOWN:
+                sprite_y += 30
 
-            paddle.moves()
+    # player 1 collision with left wall
+    if sprite_x <= 0:
+        sprite_x = 0
 
-    paddle.coordinates_1()
+    # player 1 collision with the middle
+    if sprite_x >= 450:
+        sprite_x = 450
+
+    # player 1 collision with the top
+    if sprite_y <= 0:
+        sprite_y = 0
+
+    # player 1 collision with bottom
+    if sprite_y >= 605:
+        sprite_y = 605
 
     # drawing the objects
     screen.blit(config.Background.court, config.Background.court_cord)
-    screen.blit(Players.player_1, Players.player_1_cord)
+    screen.blit(Players.player_1, (sprite_x, sprite_y))
     screen.blit(Players.player_2, Players.player_2_cord)
 
     # update screen
--- players.py
+++ players.py
@@ -1,6 +1,9 @@
 import pygame
+import config
+
 
 class Players:
+
     player_1 = pygame.image.load('img/player_1.png')
     player_1_y = 300
     player_1_x = 30
@@ -14,3 +17,31 @@ class Players:
     player_2_y = 300
     player_2_x = 900
     player_2_cord = (player_2_x, player_2_y)
+
+
+def moves():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            config.game_loop = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                Players.player_1_move_left = True
+            elif event.key == pygame.K_RIGHT:
+                Players.player_1_move_right = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                Players.player_1_move_left = False
+            elif event.key == pygame.K_RIGHT:
+                Players.player_1_move_right = False
+
+    if Players.player_1_move_left:
+        Players.player_1_x -= 7
+    else:
+        Players.player_1_x += 0
+
+        # player down movement
+    if Players.player_1_move_right:
+        Players.player_1_x += 7
+    else:
+        Players.player_1_x += 0
