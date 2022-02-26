48ce5eb2d737feb0682b3c190ce6d50f4e3c3871
Victor Albarado Braga <99058838+VictorAlbaradoBraga@users.noreply.github.com>
2022-02-25 20:05:41 -0400

    Update main.py
    - Center the scope

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ while config.game_loop:
     player_2.render(screen)
     ball_1.render(screen)
     ball_2.render(screen)
-    screen.blit(config.Scope.scope, (mx, my))
+    screen.blit(config.Scope.scope, (mx-16, my-16))
     config.display_lives(screen, config.Constants.P1_LIVE_POS, config.live_1)
     config.display_lives(screen, config.Constants.P2_LIVE_POS, config.live_2)
     # ball_1.move('player_1')
